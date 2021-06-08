3238284f6fe4812e385e960824eccfe4911d62ee
Guillaume Masclet <guillaume@masclet.net>
2021-06-08 18:54:47 +0200

    Init

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+set -ex
+
+SSH_USER=guillaume
+SSH_HOST=masclet.net
+REMOTE_PATH=/home/guillaume/www/stats/
+
+rsync -av --delete \
+    --exclude=deploy.sh \
+    --exclude=.git \
+    --exclude=.env \
+    --exclude=*.dist \
+    --exclude=.gitignore \
+    --exclude=data \
+    ./ "$SSH_USER@$SSH_HOST:$REMOTE_PATH"
